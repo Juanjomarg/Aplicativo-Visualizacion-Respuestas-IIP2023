5faf69565e51f485c4250744a92bc0cc4f424b5e
juanjomarg <juanj.martinezg96@gmail.com>
2023-07-09 14:05:15 -0500

    ||Alpha||1.2||fixed results table & reseparated from grade calculator

--- assets/commons.py
+++ assets/commons.py
@@ -5,7 +5,7 @@ LOGO_IIP='./static/logoiip.png'
 PREGUNTAS_TODAS=    ['p1','p2','p3','p4','p5','p6','p7','p8','p9','p10','p11','p12','p13','p14','p15','p16','p17','p18','p19','p20','p21','p22','p23','p24','p25','p26','p27','p28','p29','p30','p31','p32','p33','p34','p35','p36','p37','p38','p39']
 PREGUNTAS_BINARIAS= ['p1','p2','p13','p14','p15','p16',            'p19','p20',            'p23','p24','p25','p26','p27','p28','p29','p30','p31','p32','p33','p34','p35','p36','p37','p38','p39']
 PREGUNTAS_BUCLES=   ['p1','p2','p13','p14','p15','p16','p17','p18','p19','p20','p21','p22','p23','p24_1','p24_2','p24_3','p24_4','p24_5','p25','p26','p27','p28',            'p31','p32','p33','p34','p35','p36','p37','p38','p39']
-ENTIDAD_INICIAL='Universidad Distrital Francisco Jose De Caldas'
+ENTIDAD_INICIAL='Veeduria Distrital'
 PREGUNTA_INICIAL='p1'
 
 UBI_AR={
